2a52edbce76458df5481f5348f1cc397b0fb8e4f
Gabriel <gabrieldagees@gmail.com>
2021-05-08 09:25:31 -0300

    RF

--- MinasValley/src/main/java/scripts/logoDesigned.js
+++
@@ -1,8 +0,0 @@
-const tagSVG = document.querySelector('svg');
-
-
-const pathDoSVG = document.querySelector('svg path');
-const tamanhoTotalDoPath = pathDoSVG.getTotalLength();
-tagSVG.style.setProperty('--tamanhoTotalDoPath', tamanhoTotalDoPath);
-
-console.log(tamanhoTotalDoPath);
