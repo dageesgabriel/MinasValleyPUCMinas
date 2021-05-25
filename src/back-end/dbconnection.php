bbc2da10c596aba63b82b1aa3b12017699d53609
Gabriel <gabrieldagees@gmail.com>
2021-05-24 21:27:55 -0300

    Update

--- src/back-end/dbconnection.php
+++ src/back-end/dbconnection.php
@@ -1,8 +1,15 @@
 <?php
 
-$link = pg_connect("host=minasvalley.postgres.database.azure.com port=5432 dbname=minasvalley user=adm@minasvalley password=valleyValley$");
-if (!$link) {
-    die('Erro ao conectar com o banco PGSQL');
-    }
-    echo "Conectado com sucesso";
-    ?> 
+$conn = pg_connect("host=minasvalley.postgres.database.azure.com port=5432 dbname=minasvalley user=adm@minasvalley password=valleyValley$");
+
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . gethostname($conn) . PHP_EOL;
+
+?>
