56dede4a42e1747b1daccc215aeb53e6662a85e9
Gabriel <gabrieldagees@gmail.com>
2021-05-25 20:10:46 -0300

    Update

--- src/back-end/dbconnection.php
+++ src/back-end/dbconnection.php
@@ -1,8 +1,16 @@
 <?php
-if(!@($conexao=pg_connect ("host=minasvalley.postgres.database.azure.com dbname=minasvalley port=5432 user=adm@minasvalley password=valleyValley$"))) {
-   print "Não foi possível estabelecer uma conexão com o banco de dados.";
-} else {
-   pg_close ($conexao);
-   print "Conexão OK!"; 
+
+$cconn = pg_connect ("host=minasvalley.postgres.database.azure.com dbname=minasvalley port=5432 user=adm@minasvalley password=valleyValley$");
+
+if (!$conn) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
 }
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($conn) . PHP_EOL;
+
+?>
 ?>
--- src/back-end/signup.php
+++ src/back-end/signup.php
@@ -1,23 +1,52 @@
 <?php
 
-include_once('dbconnection.php');
+if(isset($_POST["criar"])){
 
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $usuario = $_POST["usuario"];
     $senha = $_POST["senha"];
-    $senhaconf = $_POST["senhaconf"]; 
+    $senhaconf = $_POST["senhaconf"];   
 
-    private $conexao;
-    $this->conexao = ConectaBanco::conexao();
+    require_once 'dbconnection.php';
+    require_once 'functions.php';
+
+    if(inputVazio($nome, $email, $usuario, $senha, $senhaconf) !== false){
+
+        header("location: ../front-end/signup.php?error=inputvazio");
+        exit();
+    }
+
+    if(usuarioInvalido($usuario) !== false){
+
+        header("location: ../front-end/signup.php?error=usuarioinvalido");
+        exit();
+    }
+
+    if(emailInvalido($email) !== false){
+
+        header("location: ../front-end/signup.php?error=emailinvalido");
+        exit();
+    }
     
-    $stmt = $this->conexao->prepare("INSERT INTO usuario (usu_nome, usu_email, usu_usuario, usu_senha) VALUES (nome, email, usuario, senha)");
-     
-        $stmt->blindParam(1, $this->usu_nome);
-        $stmt->blindParam(1, $this->usu_email);
-        $stmt->blindParam(1, $this->usu_usuario);
-        $stmt->blindParam(1, $this->usu_senha);
+    if(senhaNaoBate($senha, $senhaconf) !== false){
 
-        stmt->execute();
+        header("location: ../front-end/signup.php?error=senhanaobate");
+        exit();
+    }
 
+    if(usuarioExiste($conn, $usuario, $email) !== false){
+
+        header("location: ../front-end/signup.php?error=usuariojaexiste");
+        exit();
+    }
+
+    criarUsuario($nome, $email, $usuario, $senha);
+
+    
+}else{
+
+    header("location: ../front-end/login.php");
+    exit();
+}
 ?>
