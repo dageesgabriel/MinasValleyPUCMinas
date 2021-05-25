ee067ec7f7fc5fe213e60f1517827992305f3460
Gabriel <gabrieldagees@gmail.com>
2021-05-25 19:42:45 -0300

    Update

--- src/back-end/dbconnection.php
+++ src/back-end/dbconnection.php
@@ -1,26 +1,8 @@
 <?php
-
-class dbconnection {
-
-    public static $con;
-
-    public static function conexao(){
-
-        if(!isset(self::$con)){
-
-            $host = 'minasvalley.postgres.database.azure.com';
-            $user = 'adm@minasvalley';
-            $pass = 'valleyValley$';
-            $db = 'minasvalley';
-
-            try{
-                self::$con = new PDO("mysql:host=$host;dbname=$db;", $user, $pass);
-                self::$con->exec('SET CHARSET utf8');
-            }
-            catch(Exception $e){
-                echo $e->getMessage();
-            }
-        }
-        return self::$con;
-    }
+if(!@($conexao=pg_connect ("host=minasvalley.postgres.database.azure.com dbname=minasvalley port=5432 user=adm@minasvalley password=valleyValley$"))) {
+   print "Não foi possível estabelecer uma conexão com o banco de dados.";
+} else {
+   pg_close ($conexao);
+   print "Conexão OK!"; 
 }
+?>
