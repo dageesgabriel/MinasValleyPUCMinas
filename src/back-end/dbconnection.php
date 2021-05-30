cd79c535f9c8ba04fb542f33668a6b7c0605fa60
Eduarda Oliveira <oeduarda239@gmail.com>
2021-05-29 21:39:10 -0300

    Update

--- src/back-end/dbconnection.php
+++ src/back-end/dbconnection.php
@@ -1,10 +1,8 @@
 <?php
 $appName = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-$connStr = "host=localhost port=5432 dbname=minasvalley user=ti2cc password=ti@ccc options='--application_name=$appName'";
+$connStr = "host=localhost port=5432 dbname=minasvalley user=ti2cc password=ti@ccc";
 
 //simple check
 $conn = pg_connect($connStr);
-$result = pg_query($conn, "select * from usuario");
-var_dump(pg_fetch_all($result));
 
 ?>
