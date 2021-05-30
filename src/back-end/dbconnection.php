0c7f4de05180e669203be098fbc38f688751c776
Eduarda Oliveira <oeduarda239@gmail.com>
2021-05-29 21:35:10 -0300

    update

--- src/back-end/dbconnection.php
+++ src/back-end/dbconnection.php
@@ -1,26 +1,10 @@
 <?php
+$appName = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$connStr = "host=localhost port=5432 dbname=minasvalley user=ti2cc password=ti@ccc options='--application_name=$appName'";
 
-class dbconnection {
+//simple check
+$conn = pg_connect($connStr);
+$result = pg_query($conn, "select * from usuario");
+var_dump(pg_fetch_all($result));
 
-    public static $con;
-
-    public static function conexao(){
-
-        if(!isset(self::$con)){
-
-            $host = 'localhost';
-            $user = 'ti2cc';
-            $pass = 'ti@ccc';
-            $db = 'minasvalley';
-
-            try{
-                self::$con = new PDO("pgsql:host=$host;dbname=$db;", $user, $pass);
-                self::$con->exec('SET CHARSET utf8');
-            }
-            catch(Exception $e){
-                echo $e->getMessage();
-            }
-        }
-        return self::$con;
-    }
-}
+?>
