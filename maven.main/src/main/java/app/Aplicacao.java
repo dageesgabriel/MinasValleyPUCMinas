c531ac2807ab08f4d54674f003b1b2b2e5ef5051
Gabriel <gabrieldagees@gmail.com>
2021-05-31 11:19:26 -0300

    Update

---
+++ maven.main/src/main/java/app/Aplicacao.java
@@ -0,0 +1,24 @@
+package app;
+
+import static spark.Spark.*;
+
+import service.UsuarioService;
+
+public class Aplicacao {
+	
+	private static UsuarioService usuarioService = new UsuarioService();
+	
+    public static void main(String[] args) {
+        port(6789);
+
+        post("/usuario", (request, response) -> usuarioService.add(request, response));
+
+        get("/usuario/:id", (request, response) -> usuarioService.get(request, response));
+
+        get("/usuario/update/:id", (request, response) -> usuarioService.update(request, response));
+
+        get("/usuario/delete/:id", (request, response) -> usuarioService.remove(request, response));
+
+        get("/usuario", (request, response) -> usuarioService.getAll(request, response));
+    }
+}
